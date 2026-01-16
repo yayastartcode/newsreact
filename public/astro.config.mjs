e32202ef0e43cd75d172648ff4d48c0d001b5d61
Agus H <ordertokoviral@gmail.com>
2026-01-16 18:45:14 +0700

    fix astro ssr adapter

--- public/astro.config.mjs
+++ public/astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
     integrations: [react()],
     output: 'server',
+    adapter: node({
+        mode: 'standalone'
+    }),
     server: {
         port: 4321
     },
